5ece876d8fcd6f189618626fff701a2c235947ce
tijojose27 <tijojose27@yahoo.com>
2016-05-15 18:00:46 -0500

    added javascript custom file in my scripts made some changes to the css to give it more flare and added som signatures

--- client/main.js
+++ client/main.js
@@ -2,3 +2,9 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
+BlogPost = new Mongo.Collection("Bposts");
+
+Template.blog.helpers({
+
+})
