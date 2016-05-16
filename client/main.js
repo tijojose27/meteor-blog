a98e3623c785831152d596de1fc1c71b79a1f9a8
tijojose27 <tijojose27@yahoo.com>
2016-05-15 23:57:38 -0500

    added all the fadign effects and created a template

--- client/main.js
+++ client/main.js
@@ -5,6 +5,28 @@ import './main.html';
 
 BlogPost = new Mongo.Collection("Bposts");
 
-Template.blog.helpers({
 
-})
+Template.displayBox.helpers({
+    blog:BlogPost.find({}, {sort:{createdOn:-1 }})
+});
+
+
+Template.blogs.events({
+    'submit .js-add-blog':function(event){
+        var fName, lName, bPost;
+        fName = event.target.firstName.value;
+        lName = event.target.lastName.value;
+        bPost = event.target.blogPost.value;
+        BlogPost.insert({
+            fName:fName,
+            lName:lName,
+            bPost:bPost,
+            createdOn:new Date(),
+        });
+        var form = document.getElementById("myform");
+        form.reset();
+        $("div:hidden").show(3000);
+        console.log("First Name : "+fName+"\nLast Name : "+lName+"\nBlog : "+bPost);
+        return false;
+    }
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
+    BlogPost = new Mongo.Collection("Bposts");
 });
